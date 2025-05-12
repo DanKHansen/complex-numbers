5b765ab6e3294a5b3f489a598852f1587db4c838
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-05-12 14:28:42 +0200

    refactor

--- src/main/scala/ComplexNumber.scala
+++ src/main/scala/ComplexNumber.scala
@@ -1,17 +1,10 @@
 case class ComplexNumber(real: Double = 0, imaginary: Double = 0):
-   def +(that: ComplexNumber): ComplexNumber = ComplexNumber(real + that.real, imaginary + that.imaginary)
-   def -(that: ComplexNumber): ComplexNumber = ComplexNumber(real - that.real, imaginary - that.imaginary)
-   def *(that: ComplexNumber): ComplexNumber =
-      ComplexNumber(real * that.real - imaginary * that.imaginary, imaginary * that.real + real * that.imaginary)
-   def /(that: ComplexNumber): ComplexNumber =
-      val denom = that.real * that.real + that.imaginary * that.imaginary
-      ComplexNumber(
-        (real * that.real + imaginary * that.imaginary) / denom,
-        (imaginary * that.real - real * that.imaginary) / denom)
-   def abs: Double = math.hypot(real, imaginary)
-   def conjugate: ComplexNumber = ComplexNumber(real, -imaginary)
-
+   val (r, i) = (real, imaginary)
+   def +(c: ComplexNumber): ComplexNumber = ComplexNumber(r + c.r, i + c.i)
+   def -(c: ComplexNumber): ComplexNumber = ComplexNumber(r - c.r, i - c.i)
+   def *(c: ComplexNumber): ComplexNumber = ComplexNumber(r * c.r - i * c.i, i * c.r + r * c.i)
+   def /(c: ComplexNumber): ComplexNumber = ComplexNumber((r * c.r + i * c.i) / (c.r * c.r + c.i * c.i), (i * c.r - r * c.i) / (c.r * c.r + c.i * c.i))
+   def abs: Double = math.hypot(r, i)
+   def conjugate: ComplexNumber = ComplexNumber(r, -i)
 object ComplexNumber:
-   def exp(c: ComplexNumber): ComplexNumber =
-      val er = math.exp(c.real)
-      ComplexNumber(er * math.cos(c.imaginary), er * math.sin(c.imaginary))
+   def exp(c: ComplexNumber): ComplexNumber = ComplexNumber(math.exp(c.r) * math.cos(c.i), math.exp(c.r) * math.sin(c.i))
