b312d27d1cc9f1ab11c4908c370009ca352e7527
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-05-12 14:24:11 +0200

    refactor

--- src/main/scala/ComplexNumber.scala
+++ src/main/scala/ComplexNumber.scala
@@ -3,13 +3,15 @@ case class ComplexNumber(real: Double = 0, imaginary: Double = 0):
    def -(that: ComplexNumber): ComplexNumber = ComplexNumber(real - that.real, imaginary - that.imaginary)
    def *(that: ComplexNumber): ComplexNumber =
       ComplexNumber(real * that.real - imaginary * that.imaginary, imaginary * that.real + real * that.imaginary)
-   def /(that: ComplexNumber): ComplexNumber = ComplexNumber(
-     (real * that.real + imaginary * that.imaginary) / (that.real * that.real + that.imaginary * that.imaginary),
-     (imaginary * that.real - real * that.imaginary) / (that.real * that.real + that.imaginary * that.imaginary)
-   )
-   def abs: Double = math.sqrt(real * real + imaginary * imaginary)
+   def /(that: ComplexNumber): ComplexNumber =
+      val denom = that.real * that.real + that.imaginary * that.imaginary
+      ComplexNumber(
+        (real * that.real + imaginary * that.imaginary) / denom,
+        (imaginary * that.real - real * that.imaginary) / denom)
+   def abs: Double = math.hypot(real, imaginary)
    def conjugate: ComplexNumber = ComplexNumber(real, -imaginary)
 
 object ComplexNumber:
-   def exp(that: ComplexNumber): ComplexNumber =
-      ComplexNumber(math.exp(that.real) * math.cos(that.imaginary), math.exp(that.real) * math.sin(that.imaginary))
+   def exp(c: ComplexNumber): ComplexNumber =
+      val er = math.exp(c.real)
+      ComplexNumber(er * math.cos(c.imaginary), er * math.sin(c.imaginary))
